ac71c16de19a2610ba5eee4cfe1a181afed56967
kundup <kundup61@hotmail.com>
2024-11-12 18:57:11 -0800

    collision, angular, and speedx{y} removed and zeroouth now the car sits still

--- source/main.js
+++ source/main.js
@@ -5,7 +5,7 @@ var game;
 var graph;
 var carX = 150;
 var carY = 120;
-var carSpeedx = 4, carSpeedy = 4;
+var carSpeedx = 0, carSpeedy = 0;
 const radius = 10;
 const framePerSecond = 50;
 const trackColNumber = 20;
@@ -78,32 +78,32 @@ function moveEverything() {
     carX += carSpeedx;
     carY += carSpeedy;
 
-    var tilex = Math.floor(carX / trackwidth);
-    var tiley = Math.floor(carY / trackheight);
-    var trackindex = tilex + trackColNumber * tiley;
+    // var tilex = Math.floor(carX / trackwidth);
+    // var tiley = Math.floor(carY / trackheight);
+    // var trackindex = tilex + trackColNumber * tiley;
 
-    var preballx = carX - carSpeedx;
-    var prebally = carY - carSpeedy;
-    var pretilex = Math.floor(preballx / trackwidth);
-    var pretiley = Math.floor(prebally / trackheight);
+    // var preballx = carX - carSpeedx;
+    // var prebally = carY - carSpeedy;
+    // var pretilex = Math.floor(preballx / trackwidth);
+    // var pretiley = Math.floor(prebally / trackheight);
 
-    if (trackGrid[trackindex] === 1) {
+    // if (trackGrid[trackindex] === 1) {
 
-        if (tilex != pretilex && tiley != pretiley) {
-            carSpeedy *= -1;
-            carSpeedx *= -1;
-        }
+    //     if (tilex != pretilex && tiley != pretiley) {
+    //         carSpeedy *= -1;
+    //         carSpeedx *= -1;
+    //     }
 
-        else if (tiley != pretiley) {
-            carSpeedy *= -1;
-            carY += carSpeedy;
-        }
+    //     else if (tiley != pretiley) {
+    //         carSpeedy *= -1;
+    //         carY += carSpeedy;
+    //     }
 
-        else if (tilex != pretilex) {
-            carSpeedx *= -1;
-            carX += carSpeedx;
-        }
-    }
+    //     else if (tilex != pretilex) {
+    //         carSpeedx *= -1;
+    //         carX += carSpeedx;
+    //     }
+    // }
 }
 
 function colorRect(cordx, cordy, sizex, sizey, color) {
@@ -123,7 +123,7 @@ function drawbricks() {
 }
 
 function carDraw() {
-    angular += 0.2;
+    // angular += 0.2;
     if (carLoaded) {
         drawPicAngular(carPic, carX, carY, angular);
     }
