5a5cd9a0a29e9db6164154f9d79bc22721149744
kundup <kundup61@hotmail.com>
2024-11-12 21:15:24 -0800

    down and up button activated

--- source/main.js
+++ source/main.js
@@ -3,9 +3,9 @@ console.log("merhaba");
 
 var game;
 var graph;
-var carX = 150;
-var carY = 120;
-var carSpeedx = 0, carSpeedy = 0;
+var carX = 450;
+var carY = 300;
+var carSpeed = 0
 const radius = 10;
 const framePerSecond = 50;
 const trackColNumber = 20;
@@ -34,9 +34,22 @@ var trackGrid = [
 var carPic = document.createElement("img");
 var carLoaded = false;
 var angular = 0;
+const KEY_ARROW_UP = 38;
+const KEY_ARROW_LEFT = 37;
+const KEY_ARROW_RIGHT = 39;
+const KEY_ARROW_DOWN = 40;
 
 function keyPressed(evt) {
+    
     document.getElementById("debugging").innerHTML = "keypressed: " + evt.keyCode;
+    if (evt.keyCode == KEY_ARROW_UP) {
+        carSpeed += 1;
+
+    } else if (evt.keyCode == KEY_ARROW_DOWN) {
+        carSpeed -= 1;
+    }
+
+    evt.preventDefault();
 }
 
 function keyreleased(evt) {
@@ -75,8 +88,9 @@ function drawEverything() {
 
 function moveEverything() {
 
-    carX += carSpeedx;
-    carY += carSpeedy;
+
+    carX += Math.cos(angular) * carSpeed
+    carY += Math.sin(angular) * carSpeed
 
     // var tilex = Math.floor(carX / trackwidth);
     // var tiley = Math.floor(carY / trackheight);
@@ -141,4 +155,4 @@ function drawPicAngular(pic, posx, posy, ang) {
 
 
 
-// chapter 9 continue
+// chapter 10 continue
