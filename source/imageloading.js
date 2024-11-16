a21277c1f977533feec0fda78fec49fc43b469d2
kundup <kundup61@hotmail.com>
2024-11-16 10:55:25 -0800

    image loading got secured

--- source/imageloading.js
+++ source/imageloading.js
@@ -1,23 +1,30 @@
-var countdown = 3;
+var countdown;
 var trackWallPic = new Image();
 var trackRoadPic = new Image();
 var carPic = new Image();
 
-function loadingPicture (){      
+function loadingPicture() {
 
-    carPic.onload = countdownbypictureloading;
-    carPic.src = "player1.png";
+    var imagelist = [
 
-    trackRoadPic.onload = countdownbypictureloading;
-    trackRoadPic.src = "track_road.png";
+        { pic: trackWallPic, sourc: "track_wall.png" },
+        { pic: trackRoadPic, sourc: "track_road.png" },
+        { pic: carPic, sourc: "player1.png" },
 
-    trackWallPic.onload = countdownbypictureloading;
-    trackWallPic.src = "track_wall.png"
+    ]
+
+    countdown = imagelist.length;
+
+    for (let i = 0; i < imagelist.length; i++) {
+        imagelist[i].pic.onload = countdownbypictureloading;
+        imagelist[i].pic.src = imagelist[i].sourc;
+
+    }
 }
 
-function countdownbypictureloading (){
+function countdownbypictureloading() {
     countdown--;
-    if (countdown == 0){
+    if (countdown == 0) {
         loadingdone();
     }
 }
